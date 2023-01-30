b1e0429dd50c9d8f39d4339ee1d9bf8b38b63f53
Dan <122179463+dekker-git@users.noreply.github.com>
2023-01-30 07:51:27 -0700

    sort button completed with both states

--- script.js
+++ script.js
@@ -6,10 +6,22 @@
 
 // Data
 const account1 = {
-  owner: 'Jonas Schmedtmann',
+  owner: 'Danny Dekker',
   movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
   interestRate: 1.2, // %
   pin: 1111,
+  movementsDates: [
+    '2021-11-18T21:31:17.178Z',
+    '2021-12-23T07:42:02.383Z',
+    '2022-01-28T09:15:04.904Z',
+    '2022-04-01T10:17:24.185Z',
+    '2022-05-08T14:11:59.604Z',
+    '2022-05-27T17:01:17.194Z',
+    '2023-07-11T23:36:17.929Z',
+    '2023-07-12T10:51:36.790Z',
+  ],
+  currency: 'CAD',
+  locale: 'en-US', 
 };
 
 const account2 = {
@@ -17,6 +29,18 @@ const account2 = {
   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 1.5,
   pin: 2222,
+  movementsDates: [
+    '2019-11-01T13:15:33.035Z',
+    '2019-11-30T09:48:16.867Z',
+    '2019-12-25T06:04:23.907Z',
+    '2020-01-25T14:18:46.235Z',
+    '2020-02-05T16:33:06.386Z',
+    '2020-04-10T14:43:26.374Z',
+    '2020-06-25T18:49:59.371Z',
+    '2020-07-26T12:01:20.894Z',
+  ],
+  currency: 'USD',
+  locale: 'en-US',
 };
 
 const account3 = {
@@ -24,6 +48,18 @@ const account3 = {
   movements: [200, -200, 340, -300, -20, 50, 400, -460],
   interestRate: 0.7,
   pin: 3333,
+  movementsDates: [
+    '2021-11-18T21:31:17.178Z',
+    '2021-12-23T07:42:02.383Z',
+    '2022-01-28T09:15:04.904Z',
+    '2022-04-01T10:17:24.185Z',
+    '2022-05-08T14:11:59.604Z',
+    '2022-05-27T17:01:17.194Z',
+    '2023-07-11T23:36:17.929Z',
+    '2023-07-12T10:51:36.790Z',
+  ],
+  currency: 'USD',
+  locale: 'en-US',
 };
 
 const account4 = {
@@ -31,6 +67,18 @@ const account4 = {
   movements: [430, 1000, 700, 50, 90],
   interestRate: 1,
   pin: 4444,
+  movementsDates: [
+    '2021-11-18T21:31:17.178Z',
+    '2021-12-23T07:42:02.383Z',
+    '2022-01-28T09:15:04.904Z',
+    '2022-04-01T10:17:24.185Z',
+    '2022-05-08T14:11:59.604Z',
+    '2022-05-27T17:01:17.194Z',
+    '2023-07-11T23:36:17.929Z',
+    '2023-07-12T10:51:36.790Z',
+  ],
+  currency: 'USD',
+  locale: 'en-US',
 };
 
 const accounts = [account1, account2, account3, account4];
@@ -77,7 +125,7 @@ createUsernames(accounts);
 console.log(accounts);
 
 ////FUNCTION FOR DISPLAYING THE MOVEMENTS (DEPOSITS/WITHDRAWALS) IN THE BANKIST APP INTERFACE... displayMovements()
-const displayMovements = function(movements, sort=false) {
+const displayMovements = function(movements,  sort=false) {
 
 containerMovements.innerHTML= '';
 
@@ -207,17 +255,21 @@ btnTransfer.addEventListener('click', function(e){
 
      ///-----updating UI again------
       // display movements.
-    displayMovements(currentAccount.movements);
-    // display balance.
-    calcDisplayBalance(currentAccount.movements);
-    //display summary.
-    calcDisplaySummary(currentAccount.movements);
+    displayUI();
     //////
   } else {
     console.log('transfer invalid');
     alert('Please enter a valid username and amount!')
   }
 });
+
+//UI Updating Function
+const  displayUI = ()=> {
+  displayMovements(currentAccount.movements);
+  // display balance.
+  calcDisplayBalance(currentAccount.movements);
+  //display summary.
+  calcDisplaySummary(currentAccount.movements)};
 
 ////----------request a loan ----------------////
 ///conditions: bank will only give loan if there's been a deposit worth 10% of the loan amount.
@@ -269,7 +321,23 @@ btnClose.addEventListener('click',function(e) {
 })
 
 //------- sorting button eventhandler-------
+let sorted = false;
 btnSort.addEventListener('click',function(e){
   e.preventDefault();
-  displayMovements(currentAccount.movements, true);
-})
+  displayMovements(currentAccount.movements, !sorted);
+  sorted = !sorted;
+});
+
+
+//Implementing current date
+const now = new Date();
+const day = `${now.getDate()}`.padStart(2, 0);
+const month = `${now.getMonth() + 1}`.padStart(2, 0);
+ //0-based for months
+const year = now.getFullYear();
+const hour = now.getHours();
+const minutes = now.getMinutes();
+labelDate.textContent = `${day}/${month}/${year}, ${hour}:${minutes}`;
+// we want month/day/year
+
+console.log(now);
